cb4ffa9c0c5e245c8e897e4028ea0d1a01725866
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-02-14 16:17:13 -0700

    fixed package names

--- visko-api/src/trustlab/visko/ontology/ViskoIndividual.java
+++
@@ -1,9 +0,0 @@
-package trustlab.visko.ontology;
-
-public interface ViskoIndividual {
-	public abstract String toString();
-
-	public abstract String getFileName();
-
-	public abstract String getURI();
-}
