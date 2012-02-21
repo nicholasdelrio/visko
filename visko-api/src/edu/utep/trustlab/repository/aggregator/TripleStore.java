2c23d0a5539ea13f94838d5ce22e3c306c9ec939
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-02-21 10:05:15 -0700

    create triple store from directory of rdf files

---
+++ visko-api/src/edu/utep/trustlab/repository/aggregator/TripleStore.java
@@ -0,0 +1,52 @@
+package edu.utep.trustlab.repository.aggregator;
+	
+import java.io.File;
+import java.io.FileInputStream;
+
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.tdb.*;
+
+	public class TripleStore {
+	
+	private static String MODELNAME = "TDB-VISKO";
+	private String tsDirectory;
+	private String rdfDirectory;
+	
+	public TripleStore(String inputRDFDirectory, String tripleStoreDirectory){
+		rdfDirectory = inputRDFDirectory;
+		tsDirectory = tripleStoreDirectory;	
+		
+		if(!tsDirectory.endsWith("/"))
+			tsDirectory = tsDirectory + "/";
+	}
+
+	public String create(){
+		File storesDirectory = new File(tsDirectory + MODELNAME);
+		
+		// If it exists then clean it, else create it.
+		if (storesDirectory.exists()){
+			for(File aFile : storesDirectory.listFiles()){
+				aFile.delete();
+			}
+		}
+		else
+			storesDirectory.mkdir();
+
+		Model model = TDBFactory.createModel(MODELNAME);
+
+		// Iterate through all files and load any pml data found
+		File rdf = new File(rdfDirectory);
+		for(File aFile : rdf.listFiles()){
+			try{
+			model.read(new FileInputStream(aFile), null);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("Model now has " + model.size() + " statements.");
+		model.close();
+		
+		return storesDirectory.getAbsolutePath();
+	}
+}
