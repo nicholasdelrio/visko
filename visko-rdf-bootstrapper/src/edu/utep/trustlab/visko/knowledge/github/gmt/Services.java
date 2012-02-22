7efcd590261e8900bfc65e62ca4b35ec0ffd040a
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-02-21 18:06:13 -0700

    michael jackson

--- visko-rdf-bootstrapper/src/edu/utep/trustlab/visko/knowledge/github/gmt/Services.java
+++ visko-rdf-bootstrapper/src/edu/utep/trustlab/visko/knowledge/github/gmt/Services.java
@@ -8,63 +8,62 @@ public class Services {
 	private static final String wsdlURL = "http://iw.cs.utep.edu:8080/GMT-services/services/GMTGravityTransformersUsingParameters.GMTGravityTransformersUsingParametersPort?wsdl";
 
 	public static void main(String[] args) {
-		Repository.setRepository(NickConfigurations.getCIServer());
+		Repository.setRepository(NickConfigurations.getLocalFileSystem());
 
 		String operationName = "ESRIGriddedToContourMapPS";
 
-		/*
-		 * ServiceWriter wtr = new ServiceWriter(operationName + 1);
-		 * wtr.setLabel(operationName); wtr.setOperationName(operationName);
-		 * wtr.setWSDLURL(wsdlURL); wtr.setConceptualOperator(
-		 * "http://rio.cs.utep.edu/ciserver/ciprojects/visko/contour1.owl#contour1"
-		 * ); wtr.setSupportingToolkit(
-		 * "http://rio.cs.utep.edu/ciserver/ciprojects/visko/gmt1.owl#gmt1");
-		 * wtr.dumpViskoDocumentOnCIServer();
-		 * 
-		 * operationName = "GravityASCIIPointsToMinCurvatureESRIGridded";
-		 * ServiceWriter wtr1 = new ServiceWriter(operationName + 1);
-		 * wtr1.setWSDLURL(wsdlURL); wtr1.setLabel(operationName);
-		 * wtr1.setOperationName(operationName); wtr1.setConceptualOperator(
-		 * "http://rio.cs.utep.edu/ciserver/ciprojects/visko/grid-by-surface1.owl#grid-by-surface1"
-		 * ); wtr1.setSupportingToolkit(
-		 * "http://rio.cs.utep.edu/ciserver/ciprojects/visko/gmt1.owl#gmt1");
-		 * wtr1.dumpViskoDocumentOnCIServer();
-		 * 
-		 * operationName = "GravityASCIIPointsToNearNeightborESRIGridded";
-		 * ServiceWriter wtr4 = new ServiceWriter(operationName + 1);
-		 * wtr4.setWSDLURL(wsdlURL); wtr4.setLabel(operationName);
-		 * wtr4.setOperationName(operationName); wtr4.setConceptualOperator(
-		 * "http://rio.cs.utep.edu/ciserver/ciprojects/visko/grid-by-near-neighbor1.owl#grid-by-near-neighbor1"
-		 * ); wtr4.setSupportingToolkit(
-		 * "http://rio.cs.utep.edu/ciserver/ciprojects/visko/gmt1.owl#gmt1");
-		 * wtr4.dumpViskoDocumentOnCIServer();
-		 * 
-		 * operationName = "GravityASCIIPointsTo2DPlotPS"; ServiceWriter wtr2 =
-		 * new ServiceWriter(operationName + 1);
-		 * wtr2.setOperationName(operationName); wtr2.setWSDLURL(wsdlURL);
-		 * wtr2.setOperationName(operationName); wtr2.setLabel(operationName);
-		 * wtr2.setConceptualOperator(
-		 * "http://rio.cs.utep.edu/ciserver/ciprojects/visko/plotter1.owl#plotter1"
-		 * ); wtr2.setSupportingToolkit(
-		 * "http://rio.cs.utep.edu/ciserver/ciprojects/visko/gmt1.owl#gmt1");
-		 * wtr2.dumpViskoDocumentOnCIServer();
-		 * 
-		 * operationName = "ESRIGriddedToColoredImagePS"; ServiceWriter wtr3 =
-		 * new ServiceWriter(operationName + 1);
-		 * wtr3.setOperationName(operationName); wtr3.setWSDLURL(wsdlURL);
-		 * wtr3.setLabel(operationName); wtr3.setConceptualOperator(
-		 * "http://rio.cs.utep.edu/ciserver/ciprojects/visko/grid-to-colored-image1.owl#grid-to-colored-image1"
-		 * ); wtr3.setSupportingToolkit(
-		 * "http://rio.cs.utep.edu/ciserver/ciprojects/visko/gmt1.owl#gmt1");
-		 * wtr3.dumpViskoDocumentOnCIServer();
-		 */
+		ServiceWriter wtr = new ServiceWriter(operationName);
+		wtr.setLabel(operationName);
+		wtr.setOperationName(operationName);
+		wtr.setWSDLURL(wsdlURL);
+		wtr.setConceptualOperator("https://raw.github.com/nicholasdelrio/visko/master/visko-rdf/contourer.owl#contourer");
+		wtr.setSupportingToolkit("https://raw.github.com/nicholasdelrio/visko/master/visko-rdf/gmt.owl#gmt");
+		wtr.saveDocument();
+
+		operationName = "GravityASCIIPointsToMinCurvatureESRIGridded";
+		ServiceWriter wtr1 = new ServiceWriter(operationName);
+		wtr1.setWSDLURL(wsdlURL);
+		wtr1.setLabel(operationName);
+		wtr1.setOperationName(operationName);
+		wtr1.setConceptualOperator("https://raw.github.com/nicholasdelrio/visko/master/visko-rdf/gridder.owl#gridder");
+		wtr1.setSupportingToolkit("https://raw.github.com/nicholasdelrio/visko/master/visko-rdf/gmt.owl#gmt");
+		wtr1.saveDocument();
+
+		operationName = "GravityASCIIPointsToNearNeightborESRIGridded";
+		ServiceWriter wtr4 = new ServiceWriter(operationName);
+		wtr4.setWSDLURL(wsdlURL);
+		wtr4.setLabel(operationName);
+		wtr4.setOperationName(operationName);
+		wtr4.setConceptualOperator("http://rio.cs.utep.edu/ciserver/ciprojects/visko/gridder.owl#gridder");
+		wtr4.setSupportingToolkit("http://rio.cs.utep.edu/ciserver/ciprojects/visko/gmt.owl#gmt");
+		wtr4.saveDocument();
+
+		operationName = "GravityASCIIPointsTo2DPlotPS";
+		ServiceWriter wtr2 = new ServiceWriter(operationName);
+		wtr2.setOperationName(operationName);
+		wtr2.setWSDLURL(wsdlURL);
+		wtr2.setOperationName(operationName);
+		wtr2.setLabel(operationName);
+		wtr2.setConceptualOperator("https://raw.github.com/nicholasdelrio/visko/master/visko-rdf/plotter.owl#plotter");
+		wtr2.setSupportingToolkit("https://raw.github.com/nicholasdelrio/visko/master/visko-rdf/gmt.owl#gmt");
+		wtr2.saveDocument();
+
+		operationName = "ESRIGriddedToColoredImagePS";
+		ServiceWriter wtr3 = new ServiceWriter(operationName);
+		wtr3.setOperationName(operationName);
+		wtr3.setWSDLURL(wsdlURL);
+		wtr3.setLabel(operationName);
+		wtr3.setConceptualOperator("https://raw.github.com/nicholasdelrio/visko/master/visko-rdf/rasterer.owl#rasterer");
+		wtr3.setSupportingToolkit("https://raw.github.com/nicholasdelrio/visko/master/visko-rdf/gmt.owl#gmt");
+		wtr3.saveDocument();
+
 		operationName = "CSVToTabularASCII";
 		ServiceWriter wtr5 = new ServiceWriter(operationName);
 		wtr5.setOperationName(operationName);
 		wtr5.setWSDLURL(wsdlURL);
 		wtr5.setLabel(operationName);
-		wtr5.setConceptualOperator("http://rio.cs.utep.edu/ciserver/ciprojects/visko/csv-to-tabular-ascii.owl#csv-to-tabular-ascii");
-		wtr5.setSupportingToolkit("http://rio.cs.utep.edu/ciserver/ciprojects/visko/gmt1.owl#gmt1");
+		wtr5.setConceptualOperator("https://raw.github.com/nicholasdelrio/visko/master/visko-rdf/csv-to-tabular-ascii.owl#csv-to-tabular-ascii");
+		wtr5.setSupportingToolkit("https://raw.github.com/nicholasdelrio/visko/master/visko-rdf/gmt.owl#gmt");
 		wtr5.saveDocument();
 	}
 }
--- visko-rdf-bootstrapper/src/edu/utep/trustlab/visko/knowledge/github/gmt/Toolkits.java
+++ visko-rdf-bootstrapper/src/edu/utep/trustlab/visko/knowledge/github/gmt/Toolkits.java
@@ -7,10 +7,10 @@ import edu.utep.trustlab.visko.ontology.service.writer.ToolkitWriter;
 public class Toolkits {
 	public static void main(String[] args) {
 
-		Repository.setRepository(NickConfigurations.getCIServer());
+		Repository.setRepository(NickConfigurations.getLocalFileSystem());
 
 		String documentURL;
-		ToolkitWriter wtr1 = new ToolkitWriter("gmt1");
+		ToolkitWriter wtr1 = new ToolkitWriter("gmt");
 		wtr1.setLabel("Generic Mapping Tools");
 		System.out.println(wtr1.toRDFString());
 		documentURL = wtr1.saveDocument();
