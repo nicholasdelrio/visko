1d6dc35d4974296a456caa0063e2b95e0031459e
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-03-12 13:53:30 -0600

    removed silly complication error

--- visko-rdf-bootstrapper/src/edu/utep/trustlab/visko/knowledge/Generator3.java
+++ visko-rdf-bootstrapper/src/edu/utep/trustlab/visko/knowledge/Generator3.java
@@ -20,8 +20,6 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*
 
 package edu.utep.trustlab.visko.knowledge;
 
-import edu.utep.trustlab.repository.NickConfigurations;
-import edu.utep.trustlab.repository.Repository;
 import edu.utep.trustlab.visko.knowledge.ghostscript.GSServices;
 import edu.utep.trustlab.visko.knowledge.gmt.GMTServices;
 import edu.utep.trustlab.visko.knowledge.ncl.NCLServices;
@@ -29,7 +27,6 @@ import edu.utep.trustlab.visko.knowledge.vtk.VTKServices;
 
 public class Generator3 {
 	public static void gen(){
-		(NickConfigurations.getLocalFileSystem());
 		
 		//create services that depend on Transformers created in Generator2
 		GSServices.create();
