bb504a656fadf7b321cfa8165d8208452f6b3aab
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-03-12 13:20:45 -0600

    consolidated servlets functionalities

--- visko-webapp/src/edu/utep/trustlab/visko/web/ViskoServletManager.java
+++ visko-webapp/src/edu/utep/trustlab/visko/web/ViskoServletManager.java
@@ -74,5 +74,4 @@ public class ViskoServletManager extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
-
 }
