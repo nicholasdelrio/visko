aa0a98e8e379772742798dc1af37740fd67b9cab
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-03-16 13:50:06 -0600

    added ant target to update the uris of rdf files hosted on github by changing the user name portion in the raw urls

---
+++ visko-rdf-cybershare/src/edu/utep/trustlab/repository/SwitchRepositoryURL.java
@@ -0,0 +1,23 @@
+package edu.utep.trustlab.repository;
+
+import java.io.File;
+
+import edu.utep.trustlab.visko.util.FileUtilities;
+
+public class SwitchRepositoryURL {
+	private static String nicholasdelrio = "nicholasdelrio";
+	public static void main(String[] args){
+		String rdfDirectory = args[0];
+		String github_username = args[1];
+		
+		// Iterate through all instance data rdf files and load any pml data found
+		File rdf = new File(rdfDirectory);
+		for(File aFile : rdf.listFiles()){
+			if(aFile.getName().toLowerCase().endsWith(".owl")){
+				String contents = FileUtilities.readTextFile(aFile.getAbsolutePath());
+				String updatedContents = contents.replaceAll(nicholasdelrio, github_username);
+				FileUtilities.writeTextFile(updatedContents, aFile.getAbsolutePath());
+			}
+		}
+	}
+}
