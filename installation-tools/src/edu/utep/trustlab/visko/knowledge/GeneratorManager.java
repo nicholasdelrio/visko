6776c23ff924cf7335c12f08f0d4f6ffaf148f98
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-03-29 11:09:35 -0600

    create rdf at once now

--- installation-tools/src/edu/utep/trustlab/visko/knowledge/GeneratorManager.java
+++ installation-tools/src/edu/utep/trustlab/visko/knowledge/GeneratorManager.java
@@ -1,33 +1,62 @@
 package edu.utep.trustlab.visko.knowledge;
 
+import edu.utep.trustlab.contentManagement.AlfrescoClient;
+import edu.utep.trustlab.contentManagement.CIServer;
 import edu.utep.trustlab.contentManagement.ContentManager;
 import edu.utep.trustlab.contentManagement.LocalFileSystem;
 
 
 public class GeneratorManager {
 	public static void main(String[] args){
-		int generatorID = Integer.valueOf(args[0]);
+		
+		ContentManager.setContentManager(getContentManager(args));
+		
+		Generator1.gen();
+		Generator2.gen();
+		Generator3.gen();
+		Generator4.gen();
+	}
+	
+	public static ContentManager getContentManager(String[] args){
+		String contentManager = args[0];
+		
+		if(contentManager.equalsIgnoreCase("github"))
+			return initGitHub(args);
+		else if(contentManager.equalsIgnoreCase("cat"))
+			return initCAT(args);
+		else if(contentManager.equalsIgnoreCase("ciserver"))
+			return initCIServer(args);
+		
+		return null;
+	}
+	
+	public static ContentManager initCIServer(String[] args){
 		String serverURL = args[1];
-		String filePath = args[2];
+		String projectName = args[2];
+		String userName = args[3];
+		String password = args[4];
 		
-		LocalFileSystem fs = new LocalFileSystem(serverURL, filePath);
-		ContentManager.setContentManager(fs);
-		
-		switch(generatorID){
-			case 1: 
-				Generator1.gen();
-				break;
-			case 2: 
-				Generator2.gen();
-				break;
-			case 3: 
-				Generator3.gen();
-				break;
-			case 4:
-				Generator4.gen();
-				break;
-			default: 
-				Generator1.gen();
-		}
+		CIServer client =  new CIServer(serverURL, userName, password);
+		client.setProjectName(projectName);
+		return client;
+	}
+
+	public static ContentManager initCAT(String[] args){
+		String serverURL = args[1];
+		String projectName = args[2];
+		String userName = args[3];
+		String password = args[4];
+
+		AlfrescoClient client = new AlfrescoClient(serverURL, userName, password);
+		client.setProjectName(projectName);
+		return client;
+	}
+
+	public static ContentManager initGitHub(String[] args){
+		String serverURL = args[1];
+		String rdfFilePath = args[2];
+
+		LocalFileSystem client =  new LocalFileSystem(serverURL, rdfFilePath);
+		return client;		
 	}
 }
