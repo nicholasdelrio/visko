07014c7fc02c0cf80c9134a8e9a6147e256884d5
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-04-12 06:14:43 -0600

    generalized github url replacement with general string replacement to facilitate reuse of the code

--- installation-tools/src/edu/utep/trustlab/contentManagement/ReplaceText.java
+++ installation-tools/src/edu/utep/trustlab/contentManagement/ReplaceText.java
@@ -4,11 +4,11 @@ import java.io.File;
 
 import edu.utep.trustlab.visko.util.FileUtilities;
 
-public class SwitchGitHubBaseURL {
-	private static String nicholasdelrio = "nicholasdelrio";
+public class ReplaceText {
 	public static void main(String[] args){
 		String rdfDirectory = args[0];
 		String github_username = args[1];
+		String github_replace = args[2];
 		
 		// Iterate through all instance data rdf files and load any pml data found
 		File rdf = new File(rdfDirectory);
@@ -16,7 +16,7 @@ public class SwitchGitHubBaseURL {
 			if(aFile.getName().toLowerCase().endsWith(".owl")){
 				System.out.println("processing: " + aFile.getAbsolutePath());
 				String contents = FileUtilities.readTextFile(aFile.getAbsolutePath());
-				String updatedContents = contents.replaceAll(nicholasdelrio, github_username);
+				String updatedContents = contents.replaceAll(github_replace, github_username);
 				FileUtilities.writeTextFile(updatedContents, aFile.getAbsolutePath());
 			}
 		}
