31db7abc19d31e47c90fadc99e1512c929dde747
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-04-18 16:31:15 -0600

    adding new RDF

--- installation-tools/src/edu/utep/trustlab/visko/knowledge/GeneratorLocalFileSystem.java
+++ installation-tools/src/edu/utep/trustlab/visko/knowledge/GeneratorLocalFileSystem.java
@@ -12,38 +12,42 @@ public class GeneratorLocalFileSystem {
 		Generator1.gen();
 		System.out.println("gen1 done");
 		System.out.println("Push to GitHug now!");
+		System.out.println("*****************************************");
 		System.out.println("cd visko");
 		System.out.println("git add *");
 		System.out.println("git commit -am 'adding new RDF'");
 		System.out.println("git push <<your forked remote visko repo>>");
-		System.console().readLine("Press any key after your have pushed to GitHub.");
+		System.console().readLine("Press <<enter>> after your have pushed to GitHub.");
 		
 		Generator2.gen();
 		System.out.println("gen2 done");
 		System.out.println("Push to GitHug now!");
+		System.out.println("*****************************************");
 		System.out.println("cd visko");
 		System.out.println("git add *");
 		System.out.println("git commit -am 'adding new RDF'");
 		System.out.println("git push <<your forked remote visko repo>>");
-		System.console().readLine("Press any key after your have pushed to GitHub.");
+		System.console().readLine("Press <<enter>> after your have pushed to GitHub.");
 			
 		Generator3.gen();
 		System.out.println("gen3 done");
 		System.out.println("Push to GitHug now!");
+		System.out.println("*****************************************");
 		System.out.println("cd visko");
 		System.out.println("git add *");
 		System.out.println("git commit -am 'adding new RDF'");
 		System.out.println("git push <<your forked remote visko repo>>");
-		System.console().readLine("Press any key after your have pushed to GitHub.");
+		System.console().readLine("Press <<enter>> after your have pushed to GitHub.");
 			
 		Generator4.gen();
 		System.out.println("gen4 done");
 		System.out.println("Push to GitHug now!");
+		System.out.println("*****************************************");
 		System.out.println("cd visko");
 		System.out.println("git add *");
 		System.out.println("git commit -am 'adding new RDF'");
 		System.out.println("git push <<your forked remote visko repo>>");
-		System.console().readLine("Press any key after your have pushed to GitHub.");
+		System.console().readLine("Press <<enter>> after your have pushed to GitHub.");
 		
 		System.out.println("RDF Generation Complete!");
 
