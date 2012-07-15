20be5f94f4522b558a3054345dd605935a68137d
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-07-15 09:10:53 -0600

    contains uris to some pml engines

---
+++ api/src/edu/utep/trustlab/visko/execution/provenance/PMLResourceURI.java
@@ -0,0 +1,13 @@
+package edu.utep.trustlab.visko.execution.provenance;
+
+public final class PMLResourceURI {
+	//Inference Engines
+	public static final String ENGINE_VISKO_PARAMETER_BINDER = "https://raw.github.com/nicholasdelrio/visko-rdf/master/rdf/pml/engines/visko-parameter-binder.owl#visko-parameter-binder";
+	public static final String ENGINE_VISKO_WEB_SERVICE = "https://raw.github.com/nicholasdelrio/visko-rdf/master/rdf/pml/engines/visko-web-service.owl#visko-web-service";
+	
+	//Inference Rules
+	public static final String RULE_DIRECT_ASSERTION = "http://inference-web.org/registry/DPR/Told.owl#Told";
+	
+	//Formats
+	public static final String FORMAT_PLAIN_TEXT = "https://raw.github.com/nicholasdelrio/visko-rdf/master/rdf/formats/PLAIN.owl#PLAIN";
+}
