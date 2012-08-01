77f41c66c6a32807e526cf42ea2e2a2a0d45698d
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-08-01 13:23:34 -0600

    changed package name include "planning" package

--- visko-sadi/src/main/java/edu/utep/trustlab/visko/planning/sadi/QueryEngine.java
+++ visko-sadi/src/main/java/edu/utep/trustlab/visko/planning/sadi/QueryEngine.java
@@ -1,4 +1,4 @@
-package edu.utep.trustlab.visko.execution.sadi;
+package edu.utep.trustlab.visko.planning.sadi;
 
 import java.util.Vector;
 
@@ -10,6 +10,7 @@ import ca.wilkinsonlab.sadi.service.annotations.InputClass;
 import ca.wilkinsonlab.sadi.service.annotations.OutputClass;
 import ca.wilkinsonlab.sadi.service.simple.SimpleSynchronousServiceServlet;
 
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
@@ -17,46 +18,38 @@ import com.hp.hpl.jena.rdf.model.Resource;
 //import com.hp.hpl.jena.rdf.model.Statement;
 //import com.hp.hpl.jena.rdf.model.StmtIterator;
 
-import edu.utep.trustlab.visko.planning.Pipeline;
-import edu.utep.trustlab.visko.planning.PipelineSet;
-import edu.utep.trustlab.visko.planning.Query;
+import edu.utep.trustlab.visko.planning.*;
 import edu.utep.trustlab.visko.sparql.ViskoTripleStore;
 import edu.utep.trustlab.visko.util.ResultSetToVector;
 
-@Name("query-planner")
+@Name("planner")
 @ContactEmail("nicholas.delrio@gmail.com")
 @InputClass("https://raw.github.com/nicholasdelrio/visko-rdf/master/rdf/ontology/visko-query.owl#QueryPlanRequest")
 @OutputClass("https://raw.github.com/nicholasdelrio/visko-rdf/master/rdf/ontology/visko-query.owl#QueryPlan")
-public class QueryPlanner extends SimpleSynchronousServiceServlet
+public class QueryEngine extends SimpleSynchronousServiceServlet
 {
-	private static final Logger log = Logger.getLogger(QueryPlanner.class);
+	private static final Logger log = Logger.getLogger(QueryEngine.class);
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void processInput(Resource input, Resource output)
 	{
-		System.out.println("running QueryPlanner...");
-
 		//needed by visko pipeline generator to identify pipelines
 		ViskoTripleStore.setEndpointURL("http://iw.cs.utep.edu/visko-web/ViskoServletManager?requestType=query-triple-store");
 		ViskoTripleStore tripleStore = new ViskoTripleStore();
 		
 		//extract inputs
-		Resource dataset = input.getPropertyResourceValue(Vocab.visualizes);
+		Resource dataset = input.getPropertyResourceValue(Vocab.wasDerivedFrom);
 		String format = dataset.getProperty(Vocab.hasFormat).getObject().toString();
 		String url = dataset.getProperty(Vocab.hasURL).getObject().toString();
-		String viewerSet = dataset.getProperty(Vocab.viewedIn).getObject().toString();
-		String view = dataset.getProperty(Vocab.viewedAs).getObject().toString();
+		String viewerSet = input.getProperty(Vocab.hasResultViewableIn).getObject().toString();
 		
 		System.out.println("format: " + format);
 		System.out.println("url: " + url);
 		System.out.println("viewerSet: " + viewerSet);
-		System.out.println("view: " + view);
 		
 		//construct visko query
 		Query viskoQuery = new Query(url, format, viewerSet);
-		if(!view.endsWith("wildcard"))
-			viskoQuery.setViewURI(view);
 		
 		//execute query engine to get set of pipelines back
 		edu.utep.trustlab.visko.planning.QueryEngine engine = new edu.utep.trustlab.visko.planning.QueryEngine(viskoQuery);
@@ -76,28 +69,38 @@ public class QueryPlanner extends SimpleSynchronousServiceServlet
 				
 				currentServiceInvocation = output.getModel().createResource(Vocab.serviceInvocation);
 				setEquivalentSADIURI(currentServiceInvocation, owlsServiceURI, tripleStore);
-				
+						
 				if(previousServiceInvocation == null)
-					output.addProperty(Vocab.isFinalizedWith, currentServiceInvocation);
+					output.addProperty(Vocab.hadActivity, currentServiceInvocation);
 				
 				if(previousServiceInvocation != null){
-					//previousServiceInvocation.addProperty(Vocab.precededByInvocation, currentServiceInvocation);
-					output.getModel().add(previousServiceInvocation, Vocab.precededByInvocation, currentServiceInvocation);
+					previousServiceInvocation.addProperty(Vocab.wasInformedBy, currentServiceInvocation);
+					output.getModel().add(previousServiceInvocation, Vocab.wasInformedBy, currentServiceInvocation);
+				}
+				
+				if(i == 0){
+					Literal urlLiteral = output.getModel().createLiteral(url);
+					currentServiceInvocation.addProperty(Vocab.used, urlLiteral);
+					output.getModel().add(currentServiceInvocation, Vocab.used, urlLiteral);
 				}
 				previousServiceInvocation = currentServiceInvocation;
 			}
 		}
 		
 		System.out.println("setting prefixes...");
+		
+		//add prefixes to make rdf pretty
+		output.getModel().setNsPrefix("pml-provenance", Vocab.pmlpPrefix);
+		output.getModel().setNsPrefix("prov", Vocab.provPrefix);
 		output.getModel().setNsPrefix("visko-query", Vocab.viskoQueryPrefix);
 	}
 	
 	private static void setEquivalentSADIURI(Resource serviceInvocation, String owlsServiceURI, ViskoTripleStore tripleStore){
 		Vector<String> sadiService = ResultSetToVector.getVectorFromResultSet(tripleStore.getEquivalentSADI(owlsServiceURI), "sadiService");
 		if(sadiService.size() > 0)
-			serviceInvocation.addProperty(Vocab.isAttributedTo, sadiService.firstElement());
+			serviceInvocation.addProperty(Vocab.wasAttributedTo, sadiService.firstElement());
 		else
-			serviceInvocation.addProperty(Vocab.isAttributedTo, owlsServiceURI);
+			serviceInvocation.addProperty(Vocab.wasAttributedTo, owlsServiceURI);
 	}
 	
 	private static Pipeline findLongestPipeline(PipelineSet set){
@@ -116,22 +119,25 @@ public class QueryPlanner extends SimpleSynchronousServiceServlet
 	private static final class Vocab
 	{
 		private static Model m_model = ModelFactory.createDefaultModel();
-				
+		
 		private static final String provPrefix = "http://www.w3.org/ns/prov/";
 		private static final String viskoQueryPrefix = "https://raw.github.com/nicholasdelrio/visko-rdf/master/rdf/ontology/visko-query.owl#";
 		private static final String pmlpPrefix = "http://inference-web.org/2.0/pml-provenance.owl#";
 		
 		public static final Resource QueryPlan = m_model.createResource("https://raw.github.com/nicholasdelrio/visko-rdf/master/rdf/ontology/visko-query.owl#QueryPlan");
 		public static final Resource QueryPlanRequest = m_model.createResource("https://raw.github.com/nicholasdelrio/visko-rdf/master/rdf/ontology/visko-query.owl#QueryPlanRequest");
-				
+		
+		//prov related properties
+		public static final Resource Activity = m_model.createResource(provPrefix + "Activity");
+		public static final Property wasDerivedFrom = m_model.createProperty(provPrefix + "wasDerivedFrom");
+		public static final Property hadActivity = m_model.createProperty(provPrefix + "hadActivity");
+		public static final Property wasInformedBy = m_model.createProperty(provPrefix + "wasInformedBy");
+		public static final Property used = m_model.createProperty(provPrefix + "used");
+		public static final Property wasAttributedTo = m_model.createProperty(provPrefix + "wasAttributedTo");
+		
 		//viskoQuery properties
+		public static final Property hasResultViewableIn = m_model.createProperty(viskoQueryPrefix + "hasResultViewableIn");
 		public static final Resource serviceInvocation = m_model.createResource(viskoQueryPrefix + "ServiceInvocation");
-		public static final Property visualizes = m_model.createProperty(viskoQueryPrefix + "visualizes"); 	
-		public static final Property viewedIn = m_model.createProperty(viskoQueryPrefix + "viewedIn"); 	
-		public static final Property viewedAs = m_model.createProperty(viskoQueryPrefix + "viewedAs"); 	
-		public static final Property isAttributedTo = m_model.createProperty(viskoQueryPrefix + "isAttributedTo"); 
-		public static final Property precededByInvocation = m_model.createProperty(viskoQueryPrefix + "precededByInvocation"); 
-		public static final Property isFinalizedWith = m_model.createProperty(viskoQueryPrefix + "isFinalizedWith"); 
 		
 		//pml-p related properties
 		public static final Property hasFormat = m_model.createProperty(pmlpPrefix + "hasFormat");
