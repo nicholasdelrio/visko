ecc8dbb9036d006912fc5c528801e08ef3c996b8
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-08-16 21:50:09 -0600

    added views

--- installation-tools/src/edu/utep/trustlab/visko/installation/packages/PackageRDFWriter.java
+++ installation-tools/src/edu/utep/trustlab/visko/installation/packages/PackageRDFWriter.java
@@ -1,6 +1,7 @@
 package edu.utep.trustlab.visko.installation.packages;
 
 public interface PackageRDFWriter {
+	public void createViews();
 	public void createViewers();
 	public void createViewerSets();
 	public void createToolkit();
