27474b2bda73379ac4bfae9ef0b47a549d51e6b0
jraguirre2013 <aguirrejr@elp.rr.com>
2013-07-18 15:26:52 -0600

    Commit html2pdf service

--- visko-modules/module_vlc/src/org/openvisko/module/ModuleService.java
+++ visko-modules/module_vlc/src/org/openvisko/module/ModuleService.java
@@ -2,8 +2,13 @@ package org.openvisko.module;
 
 import javax.jws.WebParam;
 
-public class ModuleService {
-
+public class ModuleService 
+{
+	public String html2pdf(@WebParam(name="url") String url)
+	{
+		PDFConverter converter=new PDFConverter(url);
+		return converter.transform();
+	}
   // TODO: add your own methods here
   // use the @WebParam(name="xxx") annotation to declare parameters
   // for your service methods	
