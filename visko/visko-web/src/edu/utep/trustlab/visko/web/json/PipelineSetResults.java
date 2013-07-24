2c87399b887d9e4395cc1961779467eb6440af24
nicholas.delrio <nicholas.delrio@gmail.com>
2013-07-24 14:58:20 -0600

    broseph

---
+++ visko/visko-web/src/edu/utep/trustlab/visko/web/json/PipelineSetResults.java
@@ -0,0 +1,159 @@
+package edu.utep.trustlab.visko.web.json;
+
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+
+import edu.utep.trustlab.visko.execution.PipelineExecutor;
+import edu.utep.trustlab.visko.execution.PipelineExecutorJob;
+import edu.utep.trustlab.visko.planning.pipelines.Pipeline;
+import edu.utep.trustlab.visko.planning.pipelines.PipelineSet;
+
+
+public class PipelineSetResults {
+	
+	public static final String NULL_VIEW = "NULL-VIEW";
+	
+	private PipelineSet pipelines;
+	private String viewCriteria;
+	private String toolkitCriteria;
+	private String outputFormatCriteria;
+	private int maxResults;
+	
+	public PipelineSetResults(PipelineSet pipelines){
+		this.pipelines = pipelines;
+		setMaxResults();
+	}
+	
+	public PipelineSetResults() {
+		// TODO Auto-generated constructor stub
+		setMaxResults();
+	}
+	
+	private void setMaxResults(){
+		this.maxResults = -1;
+	}
+	
+	public void setMaxResults(int maxResults){
+		this.maxResults = maxResults;
+	}
+
+	public void setPipelineSet(PipelineSet pipelines){
+		this.pipelines = pipelines;
+	}
+	
+	public void setViewCriteria(String viewURI){
+		viewCriteria = viewURI;
+	}
+	
+	public void setToolkitCriteria(String toolkitURI){
+		toolkitCriteria = toolkitURI;
+	}
+	
+	public void setOutputFormatCriteria(String formatURI){
+		outputFormatCriteria = formatURI;
+	}
+	
+	private JSONObject getResultSetTemplate(){
+		JSONObject resultSet = new JSONObject();
+		try{
+			resultSet
+			.put("query", pipelines.getQuery().toString())
+			.put("inputURL", pipelines.getQuery().getArtifactURL());
+		}catch(Exception e){e.printStackTrace();}
+		
+		return resultSet;
+	}
+	
+	public String getEmptyResults(){
+		return getResultSetTemplate().toString();
+	}
+
+	private ArrayList<JSONObject> getResults(){
+		PipelineExecutor executor = new PipelineExecutor();
+		ArrayList<JSONObject> results = new ArrayList<JSONObject>();
+		Pipeline aPipeline;
+		for(int i = 0; i < pipelines.size() && i < maxResults; i ++){
+			aPipeline = pipelines.get(i);
+			if(targetPipeline(aPipeline))
+				results.add(getResult(executor, aPipeline));
+		}
+		return results;
+	}
+	
+	public String toString(){
+		ArrayList<JSONObject> results = getResults();
+		JSONObject resultSet = getResultSetTemplate();
+		try{
+			resultSet.put("results", results);
+			return resultSet.toString(4);
+		}
+		catch(Exception e){e.printStackTrace();}
+		
+		return resultSet.toString();
+	}
+	
+	private boolean targetPipeline(Pipeline aPipeline){
+		return 
+				this.doesFormatCheckOut(aPipeline) &&
+				this.doesToolkitCheckOut(aPipeline) &&
+				this.doesViewCheckOut(aPipeline);
+	}
+	
+	private boolean doesFormatCheckOut(Pipeline aPipeline){
+		boolean formatChecksOut = false;
+		
+		if(outputFormatCriteria == null)
+			formatChecksOut = true;
+		
+		else if(aPipeline.getOutputFormat() != null && aPipeline.getOutputFormat().equals(outputFormatCriteria))
+			formatChecksOut = true;
+		
+		return formatChecksOut;
+	}
+	
+	private boolean doesToolkitCheckOut(Pipeline aPipeline){
+		boolean toolkitChecksOut = false;
+		
+		if(toolkitCriteria == null)
+			toolkitChecksOut = true;
+		
+		else if(aPipeline.getToolkitThatGeneratesView() != null && aPipeline.getToolkitThatGeneratesView().equals(toolkitCriteria))
+			toolkitChecksOut = true;
+		
+		return toolkitChecksOut;
+	}
+	
+	private boolean doesViewCheckOut(Pipeline aPipeline){
+		boolean viewChecksOut = false;
+		if(viewCriteria == null)
+			viewChecksOut = true;
+		
+		else if(viewCriteria.equals(NULL_VIEW) && aPipeline.getViewURI() == null)
+			viewChecksOut = true;
+		
+		else if(viewCriteria.equals(aPipeline.getViewURI()))
+			viewChecksOut = true;
+		
+		return viewChecksOut;
+	}
+	
+	private JSONObject getResult(PipelineExecutor executor, Pipeline aPipeline){
+		PipelineExecutorJob job = new PipelineExecutorJob(aPipeline);
+		job.setProvenanceLogging(false);
+		executor.setJob(job);
+		executor.run();
+		
+		String viewerURI = aPipeline.getViewerURI();
+		String outputURL = job.getFinalResultURL();
+		
+		JSONObject aRecord = new JSONObject();
+		try{
+			aRecord
+			.put("outputURL", outputURL)
+			.put("viewerURI", viewerURI);}
+		catch(Exception e){e.printStackTrace();}
+		
+		return aRecord;
+	}
+}
