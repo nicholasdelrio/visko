214ec697d87c7cabf546af00bf3456bc63ae1581
nicholas.delrio <nicholas.delrio@gmail.com>
2013-08-16 16:01:52 -0600

    added printouts

--- visko-modules/module_vlc/src/org/openvisko/module/ModuleService.java
+++ visko-modules/module_vlc/src/org/openvisko/module/ModuleService.java
@@ -7,8 +7,11 @@ public class ModuleService
 	public String html2pdf(@WebParam(name="url") String url)
 	{
 		PDFConverter converter=new PDFConverter(url);
-		return converter.transform();
-	}
+		System.out.println("calling converter");
+		String returnURL = converter.transform();
+		System.out.println("returned from converter...passing back result");
+		return returnURL;
+}
   // TODO: add your own methods here
   // use the @WebParam(name="xxx") annotation to declare parameters
   // for your service methods	
